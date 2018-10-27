abe870a9268a5761109b8b33c76c45bea0eaf611
galina <galina@energysavvy.com>
2018-10-27 15:14:10 -0700

    add styles

--- App.js
+++ App.js
@@ -9,9 +9,9 @@ export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)} >
-        <View>
-          <AddEntry />
-        </View>
+          <View style={{ flex: 1 }}>
+            <AddEntry />
+          </View>
         </Provider>
     );
   }
